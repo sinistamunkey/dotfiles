c6d75d0660d889a12caa07365114d8a995b74612
Gary Prescott <gary.prescott@mpb.com>
2022-08-12 09:55:23 +0100

    tweaks to emacs and tmux

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -38,6 +38,7 @@
 ;; BASIC CONFIGURATION
 ;; ----------------------------------------------------
 (setq frame-background-mode 'dark)
+(setq x-select-enable-clipboard t)
 
 ;; Enable and configure editing modes
 (global-linum-mode)
@@ -85,43 +86,6 @@
 (global-set-key (kbd "C-c i") 'yas-insert-snippet)
 
 
-;; GO settings
-(setenv "GOPATH" (getenv "GOPATH"))
-(defun auto-complete-for-go ()
-
-(auto-complete-mode 1))
- (add-hook 'go-mode-hook 'auto-complete-for-go)
-
-(with-eval-after-load 'go-mode
-  (require 'go-autocomplete))
-
-(defun my-go-mode-hook ()
-  ; Use goimports instead of go-fmt
-  (setq gofmt-command "goimports")
-
-  ;; guru settings
-  (go-guru-hl-identifier-mode)
-
-  ; Call Gofmt before saving
-  (add-hook 'before-save-hook 'gofmt-before-save)
-
-  ; Customize compile command to run go build
-  (if (not (string-match "go" compile-command))
-      (set (make-local-variable 'compile-command)
-           "go build -v && go test -v && go vet"))
-
-  ; Godef jump key binding
-  (local-set-key (kbd "M-.") 'godef-jump)
-  (local-set-key (kbd "M-*") 'pop-tag-mark)
-  (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
-  (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
-  (require 'go-guru)
-  ;; (add-to-list 'load-path (concat (getenv "GOPATH") "src/github.com/dougm/goflymake"))
-  ;; (require 'go-flycheck)
-)
-
-(add-hook 'go-mode-hook 'my-go-mode-hook)
-
 ;; Custom settings
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -129,8 +93,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (go-guru go-autocomplete go-mode imenu-list py-isort json-mode editorconfig protobuf-mode yasnippet-snippets blacken pyenv-mode elpy better-defaults))))
+   '(pbcopy go-guru go-autocomplete go-mode imenu-list py-isort json-mode editorconfig protobuf-mode yasnippet-snippets blacken pyenv-mode elpy better-defaults)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
