7e5083e754df76c937a8245b4e416a924ab619aa
Gary Prescott <gary.prescott@mpb.com>
2023-05-11 12:13:04 +0100

    added plantuml

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -23,7 +23,7 @@
     fill-column-indicator
     json-mode
     magit
-    ob-mermaid
+    plantuml-mode
     nyan-mode
     python-isort
     yaml-mode
@@ -50,7 +50,10 @@
 (global-linum-mode t)               ;; Enable line numbers globally
 (tool-bar-mode -1)                  ;; Disable tool bar
 (setq linum-format "%d ")           ;; Add a bit of padding to the line numbers
+(setq window-divider-mode nil)      ;; Disable the window divider
+(scroll-bar-mode -1)                ;; Disable scroll bar (we have nyancat yo!)
 (add-hook 'after-init-hook (lambda () (load-theme 'darcula)))
+(add-to-list 'image-types 'svg)
 
 ;; Show full path of buffer in title
 (setq frame-title-format
@@ -62,13 +65,25 @@
 (setq ido-everywhere t)
 (ido-mode 1)
 
-;; Configure mermaid for Org mode
-(setq ob-mermaid-cli-path "/usr/local/bin/mmdc")
+;; Configure PlantUML mode
+(setq org-plantuml-jar-path "~/plantuml.jar")
+(setq plantuml-jar-path "~/plantuml.jar")
+(setq plantuml-default-exec-mode 'jar)
+
+;; Configure Org mode babel languages
 (org-babel-do-load-languages
-    'org-babel-load-languages
-    '((mermaid . t)
-      (scheme . t)))
-    
+ 'org-babel-load-languages
+ '((plantuml . t)))
+
+;; Auto display inline images within org mode
+(setq org-startup-with-inline-images t)
+
+;; Enforce PNG as the output type of plantuml
+(setq plantuml-output-type "png")
+
+;; Configure time display
+(display-time-mode 1)
+(setq display-time-format "%a %d %b %H:%M")
 
 ;; Nyan mode for reasons
 (nyan-mode 1)
@@ -101,6 +116,8 @@
 (setq fci-rule-column 88)           ;; Set the fill culumn indicator to 88 characters
 
 (setq elpy-rpc-virtualenv-path 'current) ;; Enforce elpy to use current virtualenv
+(setq elpy-rpc-timeout 10)  ;; Increase timeout due to slow virtualenvs and big codebases
+
 
 ;; treesitter config
 (require 'tree-sitter)
@@ -119,11 +136,22 @@
 	    (add-hook 'after-save-hook 'blacken-buffer nil 'make-it-local)))
 (add-hook 'python-mode-hook #'python-isort-on-save-mode)
 
+;; Elpy hooks
+(add-hook 'elpy-mode-hook
+	  (lambda ()
+	    (define-key elpy-mode-map (kbd "C-<return>") nil)))  ;; I keep pressing this by accident
+
 ;; JSON mode hooks
 (add-hook 'json-mode-hook
           (lambda ()
             (make-local-variable 'js-indent-level)
             (setq js-indent-level 2)))
+
+;; PlantUML mode hooks
+(add-hook 'plantuml-mode-hook
+	  (lambda ()
+	    (make-local-variable 'plantuml-indent-level)
+	    (setq plantuml-indent-level 2)))
 
 ;; ====================================
 ;; Custom helpers
@@ -164,13 +192,16 @@
  '(auto-dim-other-buffers-mode t)
  '(custom-safe-themes
    '("79586dc4eb374231af28bbc36ba0880ed8e270249b07f814b0e6555bdcb71fab" default))
+ '(display-time-mode t)
  '(elpy-rpc-python-command "python")
  '(package-selected-packages
-   '(ob-mermaid magit yaml-mode json-mode typescript-mode org-mode org-modern cython-mode auto-dim-other-buffers nyan-mode blacken tree-sitter-langs fill-column-indicator elpy better-defaults))
- '(python-shell-interpreter "python"))
+   '(plantuml-mode ob-mermaid magit yaml-mode json-mode typescript-mode org-mode org-modern cython-mode auto-dim-other-buffers nyan-mode blacken tree-sitter-langs fill-column-indicator elpy better-defaults))
+ '(python-shell-interpreter "python")
+ '(tool-bar-mode nil)
+ '(warning-suppress-types '(((python python-shell-completion-native-turn-on-maybe)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#2B2B2B" :foreground "#a9b7c6" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo")))))
