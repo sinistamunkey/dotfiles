8f3a6e2fdcd7f38a53b81858430ec8e373ec6f1d
Gary Prescott <gary.prescott@mpb.com>
2023-08-03 11:41:33 +0100

    increased font size

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -183,4 +183,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2B2B2B" :foreground "#a9b7c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "nil" :family "Menlo")))))
